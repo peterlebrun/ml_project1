6566c36e77f820cac633bfdca12e95beed8293f6
Peter LeBrun <peterlebrun@gmail.com>
2015-01-27 23:21:45 -0500

    First decision tree created

--- test1.py
+++ test1.py
@@ -4,10 +4,11 @@ import pandas as pd
 import sys
 import numpy as np
 from sklearn.cross_validation import train_test_split
+from sklearn import tree
 import numpy as np
 
-print 'Python version: ' + sys.version
-print 'Pandas version: ' + pd.__version__
+#print 'Python version: ' + sys.version
+#print 'Pandas version: ' + pd.__version__
 
 p = read_csv('data_banknote_authentication.txt',
              names=['wti_var', 'wti_skew', 'wti_curt', 'i_entr', 'cl'])
@@ -17,19 +18,17 @@ p = read_csv('data_banknote_authentication.txt',
 # plt.plot(p['wti_curt']) - split on 0, 5
 # plt.plot(p['i_entr']) - split on 0
 
-p['var_lte_0'] = p['wti_var'] <= 0
-p
+p['var_lte_0']  = p['wti_var'] <= 0
+p['skew_lte_0'] = p['wti_skew'] <= 0
+p['curt_lte_0'] = p['wti_curt'] <= 0
+p['entr_lte_0'] = p['i_entr'] <= 0
 
-#p_train, p_test = train_test_split(p, test_size = 0.33)
-#
-#arr = p_train[:,3]
-#
-##plt.plot(arr)
-#
-#from sklearn import tree
-#x_val = p_train[:,:3]
-#y_val = p_train[:,4]
-#
-#clf = tree.DecisionTreeClassifier()
-#clf = clf.fit(x_val, y_val)
-#clf.predict(p_test[:,:3]).sum()
+train, test = train_test_split(p, test_size = 0.33)
+
+x_val = train[:,5:]
+y_val = train[:,4]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_val, y_val)
+print(clf)
+print(clf.predict(test[:,5:]).sum())
