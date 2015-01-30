e927908739e349bc8983e511a3518cfb05e6060f
Peter LeBrun <peter.lebrun@americanwell.com>
2015-01-30 09:08:28 -0500

    Update

--- test1.py
+++ test1.py
@@ -130,7 +130,7 @@
 #    clf.fit(X, y)
 #
 ##Plot the decision boundary.  For that we will assign a color
-##to each point in the mesh [x_min, m_max]x[y_min, y_max]
+##to each point in the mesh [x_min, x_max] x [y_min, y_max]
 #
 #    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 #    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
